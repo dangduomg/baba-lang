22d448bc0fa5f5f4e54a545ceae5c4efb6686183
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-03-06 21:09:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 from time import sleep
-
 import operator
+import logging
+
 from lark import Lark
 from lark.visitors import Interpreter
 from ast import literal_eval
 
 
-VERSION = '0.2.2'
+VERSION = '0.2.5'
 ABOUT = f'''\
 this is babalang, (C) 2024
 version {VERSION}
@@ -19,65 +20,70 @@ pl_parser = Lark(r'''
 
 body: stmt*
 
-?stmt: print_stmt
-     | about_stmt
-     | var_stmt
-     | python_call_stmt
+?stmt: cmd
      | label
      | goto_stmt
-     | goto_if_stmt
-     | python_exec_stmt
      | function_stmt
-     | call_stmt
-     | call_and_save_stmt
+     | if_stmt
+     | if_else_stmt
+     | while_stmt
+     | do_while_stmt
+     | for_stmt
+     | break_stmt
+     | continue_stmt
      | return_stmt
-     | sleep_stmt
-     | exit_stmt
-     | input_stmt
-     | nonlocal_var_stmt
+     | expr_stmt
 
-print_stmt: "\\print" value ";"
+?cmd: "\\print" expr ";"                           -> print_stmt
+    | "\\about" ";"                                -> about_stmt
+    | "\\set" IDENT "," expr ";"                   -> var_stmt
+    | "\\py_call" IDENT "," string ("," expr)* ";" -> python_call_stmt
+    | "\\py_exec" string ";"                       -> python_exec_stmt
+    | "\\goto" IDENT ";"                           -> goto_stmt
+    | "\\goto" IDENT _IF "(" expr ")" ";"          -> goto_if_stmt
+    | "\\call" expr ("," expr)* ";"                -> call_stmt
+    | "\\callsave" IDENT "," expr ("," expr)* ";"  -> call_and_save_stmt
+    | "\\sleep" expr ";"                           -> sleep_stmt
+    | "\\exit" expr ";"                            -> exit_stmt
+    | "\\input" IDENT "," expr ";"                 -> input_stmt
+    | "\\nonlocal_set" IDENT "," expr ";"          -> nonlocal_var_stmt
 
-about_stmt: "\\about" ";"
+label: IDENT ":"
 
-var_stmt: "\\set" CNAME "," value ";"
+goto_stmt: _GOTO IDENT ";"
 
-python_call_stmt: "\\py_call" CNAME "," string ("," value)* ";"
+function_stmt: _FUNCTION IDENT "(" args ")" "{" body "}"
+args: (IDENT ("," IDENT)*)?
 
-label: CNAME ":"
+return_stmt: _RETURN expr ";"
 
-goto_stmt: "\\goto" CNAME ";"
+if_stmt: _IF "(" expr ")" "{" body "}"
 
-goto_if_stmt: "\\goto" CNAME "if" "(" value ")" ";"
+if_else_stmt: _IF "(" expr ")" "{" body "}" _ELSE (if_else_stmt | if_stmt | "{" body "}")
 
-python_exec_stmt: "\\py_exec" string ";"
+while_stmt: _WHILE "(" expr ")" "{" body "}"
 
-function_stmt: "function" CNAME "(" args ")" "{" body "}"
-args: (CNAME ("," CNAME)*)?
+do_while_stmt: _DO "{" body "}" _WHILE "(" expr ")"
 
-call_stmt: "\\call" value ("," value)* ";"
+for_stmt: _FOR "(" expr ";" expr ";" expr ")" "{" body "}"
 
-call_and_save_stmt: "\\callsave" CNAME "," value ("," value)* ";"
+break_stmt: _BREAK ";"
+continue_stmt: _CONTINUE ";"
 
-return_stmt: "return" value ";"
+expr_stmt: expr ";"
 
-sleep_stmt: "\\sleep" value ";"
+?expr: assign
 
-exit_stmt: "\\exit" ";"
+?assign: assign_expr "=" assign
+       | cmp
 
-input_stmt: "\\input" CNAME "," value ";"
-
-nonlocal_var_stmt: "\\nonlocal_set" CNAME "," value ";"
-
-?value: rhs_expr
-
-?rhs_expr: cmp
+?assign_expr: IDENT -> var
 
 ?cmp: sum "==" sum -> eq
     | sum "!=" sum -> ne
-    | sum "<" sum -> lt
+    | sum "<" sum  -> lt
     | sum "<=" sum -> le
-    | sum ">" sum -> gt
+    | sum ">" sum  -> gt
     | sum ">=" sum -> ge
     | sum
 
@@ -85,26 +91,61 @@ nonlocal_var_stmt: "\\nonlocal_set" CNAME "," value ";"
     | sum "-" prod -> sub
     | prod
 
-?prod: prod "*" atom -> mul
-     | prod "/" atom -> div
-     | prod "%/%" atom -> floordiv
-     | prod "%" atom -> mod
-     | atom
+?prod: prod "*" prefix   -> mul
+     | prod "/" prefix   -> div
+     | prod "%/%" prefix -> floordiv
+     | prod "%" prefix   -> mod
+     | prefix
+
+?prefix: "+" prefix -> pos
+       | "-" prefix -> neg
+       | power
+
+?power: postfix "**" prefix -> pow
+      | postfix
+
+?postfix: postfix "(" (expr ("," expr)*)? ")" -> call
+        | atom
 
 ?atom: literal
-     | CNAME -> var
-     | "(" rhs_expr ")"
+     | IDENT -> var
+     | "(" expr ")"
 
 ?literal: string
-        | SIGNED_INT -> int
-        | SIGNED_FLOAT -> float
+        | INT -> int
+        | FLOAT -> float
+        | _TRUE -> true
+        | _FALSE -> false
+        | _NULL -> null
 
-string: ESCAPED_STRING
+string: STRING_DQUOTE
+      | STRING_SQUOTE
+STRING_DQUOTE: "\"" _STRING_ESC_INNER "\""
+STRING_SQUOTE: "'" _STRING_ESC_INNER "'"
+_STRING_INNER: /.*?/
+_STRING_ESC_INNER: _STRING_INNER /(?<!\\)(\\\\)*?/
+
+IDENT: /(?!/ RESERVED  /)/ CNAME
+
+RESERVED: _TRUE | _FALSE | _NULL | _FUNCTION | _RETURN | _IF | _ELSE | _WHILE | _GOTO | _DO | _FOR | _BREAK | _CONTINUE
+_TRUE: "True"
+_FALSE: "False"
+_NULL: "None"
+_FUNCTION: "function"
+_RETURN: "return"
+_IF: "if"
+_ELSE: "else"
+_WHILE: "while"
+_GOTO: "goto"
+_DO: "do"
+_FOR: "for"
+_BREAK: "break"
+_CONTINUE: "continue"
 
 %import common.CNAME
 %import common.ESCAPED_STRING
-%import common.SIGNED_INT
-%import common.SIGNED_FLOAT
+%import common.INT
+%import common.FLOAT
 %import common.SH_COMMENT
 %import common.C_COMMENT
 %import common.CPP_COMMENT
@@ -126,6 +167,12 @@ class PLReturn(PLCtrlException):
 class PLJump(PLCtrlException):
     pass
 
+class PLBreak(PLCtrlException):
+    pass
+
+class PLContinue(PLCtrlException):
+    pass
+
 
 class PLFrame:
     def __init__(self, vars, env):
@@ -134,7 +181,7 @@ class PLFrame:
 
     def var_search(self, name):
         if name in self.vars:
-                return self.vars[name]
+            return self.vars[name]
         for scope in self.env:
             if name in scope.vars:
                 return scope.vars[name]
@@ -147,7 +194,7 @@ class PLFrame:
 
     def var_search_set(self, name, value):
         if name in self.vars:
-                return self.var_set(name, value)
+            return self.var_set(name, value)
         for scope in self.env:
             if name in scope.vars:
                 return scope.var_set(name, value)
@@ -163,21 +210,29 @@ class PLFunction:
         self.env = env
 
     def __repr__(self):
-        return f'babalang function {self.name}({", ".join(form_args)})'
+        return f'<baba-lang function {self.name}({", ".join(self.form_args)})>'
 
+
+def unary_operator(f):
+    def operator(self, tree):
+        a, = tree.children
+        return f(self.visit(a))
+    operator.__name__ = f.__name__
+    return operator
 
 def binary_operator(f):
     def operator(self, tree):
         a, b = tree.children
         return f(self.visit(a), self.visit(b))
     operator.__name__ = f.__name__
-    operator.__doc__ = f.__doc__
     return operator
 
 
 class PLInterpreter(Interpreter):
     def __init__(self):
-        self.call_stack = [PLFrame({}, None)]
+        self.call_stack = [PLFrame({}, [])]
+
+    # ----- helpers -----
 
     def push_frame(self, *args, **kwargs):
         self.call_stack.append(PLFrame(*args, **kwargs))
@@ -248,7 +303,7 @@ class PLInterpreter(Interpreter):
 
     def goto_stmt(self, tree):
         '''Raises an exception to jump to a label'''
-        label, = tree.children;
+        label, = tree.children
         raise PLJump(label)
 
     def goto_if_stmt(self, tree):
@@ -274,16 +329,128 @@ class PLInterpreter(Interpreter):
         func, *args = tree.children
         func = self.visit(func)
         args = [self.visit(a) for a in args]
-        self._call_stmt(func, args)
+        self._call(func, args)
 
     def call_and_save_stmt(self, tree):
         '''Calls a function and save its result to a variable'''
         varname, func, *args = tree.children
         func = self.visit(func)
         args = [self.visit(a) for a in args]
-        self.var_set(varname.value, self._call_stmt(func, args))
+        self.var_set(varname.value, self._call(func, args))
 
-    def _call_stmt(self, func, spec_args):
+    def return_stmt(self, tree):
+        '''Raises an exception to return control'''
+        value, = tree.children
+        raise PLReturn(self.visit(value))
+
+    def sleep_stmt(self, tree):
+        '''Sleeps for a certain amount of time'''
+        time, = tree.children
+        sleep(self.visit(time))
+
+    def exit_stmt(self, tree):
+        '''Exits the interpreter'''
+        exitcode, = tree.children
+        sys.exit(exitcode)
+
+    def input_stmt(self, tree):
+        '''Get input from the user'''
+        varname, prompt = tree.children
+        self.var_set(varname.value, input(self.visit(prompt)))
+
+    def if_stmt(self, tree):
+        '''Executes the body if the condition is true'''
+        cond, body = tree.children
+        if self.visit(cond):
+            self.visit(body)
+
+    def if_else_stmt(self, tree):
+        '''Executes body 1 if the condition is true, otherwise body 2'''
+        cond, body1, body2 = tree.children
+        if self.visit(cond):
+            self.visit(body1)
+        else:
+            self.visit(body2)
+
+    def while_stmt(self, tree):
+        '''Executes the body *while* the condition is true'''
+        cond, body = tree.children
+        self._loop(cond, body)
+
+    def do_while_stmt(self, tree):
+        '''Same as while_stmt, but executes the body first before evaluating the condition'''
+        cond, body = tree.children
+        self.visit(body)
+        self._loop(cond, body)
+
+    def for_stmt(self, tree):
+        '''C for loop'''
+        start, cond, step, body = tree.children
+        self.visit(start)
+        self._loop(cond, body, step)
+
+    def break_stmt(self, tree):
+        '''Breaks out of a loop'''
+        raise PLBreak
+
+    def continue_stmt(self, tree):
+        '''Skip the rest of the loop body'''
+        raise PLContinue
+
+    def _loop(self, cond, body, step=None):
+        while self.visit(cond):
+            try:
+                self.visit(body)
+            except PLBreak:
+                break
+            except PLContinue:
+                continue
+            except Exception as e:
+                raise e
+            finally:
+                if step:
+                    self.visit(step)
+
+    def expr_stmt(self, tree):
+        '''Expression statement'''
+        expr, = tree.children
+        return self.visit(expr)
+
+    def assign(self, tree):
+        assign_expr, value = tree.children
+        value = self.visit(value)
+        if assign_expr.data == 'var':
+            name, = assign_expr.children
+            self.var_set(name.value, value)
+
+    def _nonlocal_assign(self, assign_expr, value):
+        if assign_expr.data == 'var':
+            name, = assign_expr.children
+            self.var_search_set(name.value, value)
+
+    eq = binary_operator(operator.eq)
+    ne = binary_operator(operator.ne)
+    lt = binary_operator(operator.lt)
+    le = binary_operator(operator.le)
+    gt = binary_operator(operator.gt)
+    ge = binary_operator(operator.ge)
+    add = binary_operator(operator.add)
+    sub = binary_operator(operator.sub)
+    mul = binary_operator(operator.mul)
+    div = binary_operator(operator.truediv)
+    floordiv = binary_operator(operator.floordiv)
+    mod = binary_operator(operator.mod)
+    pos = unary_operator(operator.pos)
+    neg = unary_operator(operator.neg)
+    pow = binary_operator(operator.pow)
+
+    def call(self, tree):
+        func, *spec_args = tree.children
+        func = self.visit(func)
+        spec_args = [self.visit(a) for a in spec_args]
+        self._call(func, spec_args)
+
+    def _call(self, func, spec_args):
         if len(spec_args) < len(func.form_args):
             raise RuntimeError('too little arguments!')
         elif len(spec_args) > len(func.form_args):
@@ -301,38 +468,6 @@ class PLInterpreter(Interpreter):
         finally:
             self.pop_frame()
         return retval
-
-    def return_stmt(self, tree):
-        '''Returns a value'''
-        value, = tree.children
-        raise PLReturn(self.visit(value))
-
-    def sleep_stmt(self, tree):
-        '''Sleeps for a certain amount of time'''
-        time, = tree.children
-        sleep(self.visit(time))
-
-    def exit_stmt(self, tree):
-        '''Exits the interpreter'''
-        sys.exit(0)
-
-    def input_stmt(self, tree):
-        '''Get input from the user'''
-        varname, prompt = tree.children
-        self.var_set(varname.value, input(self.visit(prompt)))
-
-    eq = binary_operator(operator.eq)
-    ne = binary_operator(operator.ne)
-    lt = binary_operator(operator.lt)
-    le = binary_operator(operator.le)
-    gt = binary_operator(operator.gt)
-    ge = binary_operator(operator.ge)
-    add = binary_operator(operator.add)
-    sub = binary_operator(operator.sub)
-    mul = binary_operator(operator.mul)
-    div = binary_operator(operator.truediv)
-    floordiv = binary_operator(operator.floordiv)
-    mod = binary_operator(operator.mod)
 
     def var(self, tree):
         '''Variable reference'''
@@ -354,6 +489,18 @@ class PLInterpreter(Interpreter):
         f, = tree.children
         return literal_eval(f)
 
+    def true(self, tree):
+        '''Boolean "True"'''
+        return True
+
+    def false(self, tree):
+        '''Boolean "False"'''
+        return False
+
+    def null(self, tree):
+        '''Null value'''
+        return None
+
 pl_interpreter = PLInterpreter()
 
 
@@ -363,14 +510,19 @@ def interpret(pl_string):
 
 
 def main(args):
+    
     if len(args) > 1:
         file = args[1]
         with open(file) as f:
             interpret(f.read())
     else:
+        logging.basicConfig(format='%(levelname)s: %(message)s')
         while True:
             inpt = input('> ')
-            interpret(inpt)
+            try:
+                interpret(inpt)
+            except Exception as e:
+                logging.error(e)
 
 if __name__ == '__main__':
     main(sys.argv)
