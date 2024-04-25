d08d1f81af1328f4d2cc9c5baf24c5b5f0e95b6d
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-04-25 16:32:15 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,2 +1,6 @@
+import main
+import intr_classes
+from state import State()
+
 def test_expression():
-    assert interpret("1 + 1").eq(intr_classes.Int(2))
+    assert main.interpret("1 + 1", State()).eq(intr_classes.Int(2))
