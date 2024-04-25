ef6640398a66484e87c04145c626af45fe6c9f2d
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-04-25 20:30:36 +0700

    add test_variable test case

--- test_main.py
+++ test_main.py
@@ -1,6 +1,16 @@
+from pytest import fixture
+
 import main
 import intr_classes
 from state import State
 
+@fixture
+def example_state():
+    return State()
+
 def test_expression():
-    assert main.interpret_expr("1 + 1", State()).eq(intr_classes.Int(2))
+    assert main.interpret_expr("1 + 1", example_state).eq(intr_classes.Int(2))
+
+def test_variable():
+    main.interpret("a = 3;", example_state)
+    assert example_state.get_var('a').eq(intr_classes.Int(3))
