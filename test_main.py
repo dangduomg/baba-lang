fd6bbce0bd05d7fdc91c36cfb4a204879ae399b1
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-04-25 20:33:41 +0700

    add test_variable case

--- test_main.py
+++ test_main.py
@@ -8,9 +8,9 @@ from state import State
 def example_state():
     return State()
 
-def test_expression():
+def test_expression(example_state):
     assert main.interpret_expr("1 + 1", example_state).eq(intr_classes.Int(2))
 
-def test_variable():
+def test_variable(example_state):
     main.interpret("a = 3;", example_state)
     assert example_state.get_var('a').eq(intr_classes.Int(3))
