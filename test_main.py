22b0880a014c898f8edb3969d290cff39e6f7f53
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-04-25 23:21:52 +0700

    bugfix

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ def test_expression(example_state):
     assert main.interpret_expr("1 + 1", example_state).eq(intr_classes.Int(2))
 
 def test_variable(example_state):
-    main.interpret("a = 3", example_state)
+    main.interpret("a = 3;", example_state)
     assert example_state.get_var('a').eq(intr_classes.Int(3))
 
 def test_if(example_state):
