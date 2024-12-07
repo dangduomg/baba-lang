6870d1e2b0e82bd8e158125034d4e4b01075dc53
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-07 14:50:19 +0700

    separate visit to visit and visit_expr

--- src/ast_interp.py
+++ src/ast_interp.py
@@ -37,10 +37,22 @@ class Int(Value):
 
 
 class ASTInterpreter(ASTVisitor):
+    """AST interpreter"""
 
     def visit(self, node: ast_classes._AstNode) -> Result:
+        #pylint: disable=protected-access
         match node:
-            case ast_classes.BinaryOp(left=left, op=op, right=right):
-                return self.visit(left).binary_op(op, self.visit(right))
+            case ast_classes._Expr():
+                return self.visit_expr(node)
+        return error_not_implemented
+
+    def visit_expr(self, node: ast_classes._Expr) -> ExpressionResult:
+        """Visit an expression node"""
+        match node:
+            case ast_classes.BinaryOp(meta=meta, left=left, op=op, right=right):
+                return self.visit_expr(left).binary_op(op, self.visit_expr(right), meta)
+            case ast_classes.Prefix(meta=meta, op=op, operand=operand):
+                return self.visit_expr(operand).unary_op(op, meta)
             case ast_classes.Int(value=value):
                 return Int(value)
+        return error_not_implemented
