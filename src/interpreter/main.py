14b17fa216e4203c65c94499e85d25564cd461ca
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-08 22:57:49 +0700

    Update main.py

--- src/interpreter/main.py
+++ src/interpreter/main.py
@@ -1,8 +1,8 @@
 """AST interpreter"""
 
 
-import ast_classes
-from ast_base import ASTVisitor
+from bl_ast import nodes
+from bl_ast.base import ASTVisitor
 
 from . import values
 from .base import Result, ExpressionResult, error_not_implemented
@@ -14,12 +14,12 @@ from .base import Result, ExpressionResult, error_not_implemented
 class ASTInterpreter(ASTVisitor):
     """AST interpreter"""
 
-    def visit(self, node: ast_classes._AstNode) -> Result:
+    def visit(self, node: nodes._AstNode) -> Result:
         #pylint: disable=protected-access
         match node:
             # case ast_classes._Stmt():
             #     return self.visit_stmt(node)
-            case ast_classes._Expr():
+            case nodes._Expr():
                 return self.visit_expr(node)
         return error_not_implemented
 
@@ -32,28 +32,28 @@ class ASTInterpreter(ASTVisitor):
     #             return Success()
     #     return error_not_implemented
 
-    def visit_expr(self, node: ast_classes._Expr) -> ExpressionResult:
+    def visit_expr(self, node: nodes._Expr) -> ExpressionResult:
         """Visit an expression node"""
         match node:
-            case ast_classes.BinaryOp(meta=meta, left=left, op=op, right=right):
+            case nodes.BinaryOp(meta=meta, left=left, op=op, right=right):
                 return self.visit_expr(left).binary_op(op, self.visit_expr(right), meta)
-            case ast_classes.Subscript(meta=meta, subscriptee=subscriptee, index=index):
+            case nodes.Subscript(meta=meta, subscriptee=subscriptee, index=index):
                 return self.visit_expr(subscriptee).get_item(self.visit_expr(index), meta)
-            case ast_classes.Prefix(meta=meta, op=op, operand=operand):
+            case nodes.Prefix(meta=meta, op=op, operand=operand):
                 return self.visit_expr(operand).unary_op(op, meta)
-            case ast_classes.String(value=value):
+            case nodes.String(value=value):
                 return values.String(value)
-            case ast_classes.Int(value=value):
+            case nodes.Int(value=value):
                 return values.Int(value)
-            case ast_classes.Float(value=value):
+            case nodes.Float(value=value):
                 return values.Float(value)
-            case ast_classes.TrueLiteral():
+            case nodes.TrueLiteral():
                 return values.BOOLS[True]
-            case ast_classes.FalseLiteral():
+            case nodes.FalseLiteral():
                 return values.BOOLS[False]
-            case ast_classes.NullLiteral():
+            case nodes.NullLiteral():
                 return values.NULL
-            case ast_classes.List(elems=elems_in_ast):
+            case nodes.List(elems=elems_in_ast):
                 elems = []
                 for e in elems_in_ast:
                     e_visited = self.visit_expr(e)
@@ -61,7 +61,7 @@ class ASTInterpreter(ASTVisitor):
                         return e_visited
                     elems.append(e_visited)
                 return values.BLList(elems)
-            case ast_classes.Dict(pairs=pairs):
+            case nodes.Dict(pairs=pairs):
                 content = {}
                 for pair in pairs:
                     k_visited = self.visit(pair.key)
