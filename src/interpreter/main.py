6d90cba51f32d3b56e97b2f0a4d8ce0d2d76f246
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-08 21:32:44 +0700

    move ast_interp to interpreter folder as main.py

--- src/interpreter/main.py
+++ src/interpreter/main.py
@@ -4,8 +4,8 @@
 import ast_classes
 from ast_base import ASTVisitor
 
-import interp_classes
-from interp_classes import Result, ExpressionResult, error_not_implemented
+from . import values
+from .base import Result, ExpressionResult, error_not_implemented
 
 
 #pylint: disable=too-many-return-statements
@@ -42,30 +42,30 @@ class ASTInterpreter(ASTVisitor):
             case ast_classes.Prefix(meta=meta, op=op, operand=operand):
                 return self.visit_expr(operand).unary_op(op, meta)
             case ast_classes.String(value=value):
-                return interp_classes.String(value)
+                return values.String(value)
             case ast_classes.Int(value=value):
-                return interp_classes.Int(value)
+                return values.Int(value)
             case ast_classes.Float(value=value):
-                return interp_classes.Float(value)
+                return values.Float(value)
             case ast_classes.TrueLiteral():
-                return interp_classes.Bool(True)
+                return values.BOOLS[True]
             case ast_classes.FalseLiteral():
-                return interp_classes.Bool(False)
+                return values.BOOLS[False]
             case ast_classes.NullLiteral():
-                return interp_classes.Null()
+                return values.NULL
             case ast_classes.List(elems=elems_in_ast):
                 elems = []
                 for e in elems_in_ast:
                     e_visited = self.visit_expr(e)
-                    if not isinstance(e_visited, interp_classes.Value):
+                    if not isinstance(e_visited, values.Value):
                         return e_visited
                     elems.append(e_visited)
-                return interp_classes.List(elems)
+                return values.List(elems)
             case ast_classes.Dict(pairs=pairs):
                 content = {}
                 for pair in pairs:
                     k_visited = self.visit(pair.key)
                     v_visited = self.visit(pair.value)
                     content[k_visited] = v_visited
-                return interp_classes.Dict(content)
+                return values.Dict(content)
         return error_not_implemented
