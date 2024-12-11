dc4d9a4c4c284e78c9186ca44c0d9e8ce1dd18be
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-11 10:46:35 +0700

    make main.py usable

--- src/main.py
+++ src/main.py
@@ -6,26 +6,40 @@ video game "Baba is You".
 
 
 import sys
-import logging
 from argparse import ArgumentParser
 
-import info
+from bl_ast import parse_to_ast, expr_parser
+from interpreter import ASTInterpreter
+from interpreter.base import Result, ExpressionResult
 
 
 argparser = ArgumentParser()
-argparser.add_argument('filename')
+argparser.add_argument(
+    'filename',
+    nargs='?',
+)
+
+
+default_interp = ASTInterpreter()
 
 
 def interpret(src: str, state):
     raise NotImplementedError('TODO')
 
 
-def interpret_expr(src: str, state):
-    raise NotImplementedError('TODO')
+def interpret_expr(src: str, interpreter: ASTInterpreter = default_interp) -> Result:
+    return interpreter.visit(parse_to_ast(src, expr_parser))
 
 
-def main(args: list[str]) -> None:
-    raise NotImplementedError('TODO')
+def main() -> None:
+    args = argparser.parse_args()
+    if args.filename is None:
+        src_stream = sys.stdin
+    else:
+        src_stream = open(args.filename, encoding='utf-8')
+    with src_stream:
+        src = src_stream.read()
+    print(interpret_expr(src))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
