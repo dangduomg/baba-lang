b105b172b6ca9c65af5a8307cdde4daf3f3b2b1b
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-16 09:57:57 +0700

    support both script and expression parsing

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from argparse import ArgumentParser
 
 from bl_ast import parse_to_ast, expr_parser
 from interpreter import ASTInterpreter
-from interpreter.base import Result, ExpressionResult
+from interpreter.base import Result
 
 
 argparser = ArgumentParser()
@@ -18,20 +18,27 @@ argparser.add_argument(
     'filename',
     nargs='?',
 )
+argparser.add_argument(
+    '-e', '--expression',
+    action='store_true',
+)
 
 
 default_interp = ASTInterpreter()
 
 
-def interpret(src: str, state):
-    raise NotImplementedError('TODO')
+def interpret(src: str, interpreter: ASTInterpreter = default_interp) -> Result:
+    """Interpret a script"""
+    return interpreter.visit(parse_to_ast(src))
 
 
 def interpret_expr(src: str, interpreter: ASTInterpreter = default_interp) -> Result:
+    """Interpret an expression"""
     return interpreter.visit(parse_to_ast(src, expr_parser))
 
 
 def main() -> None:
+    """Main function"""
     args = argparser.parse_args()
     if args.filename is None:
         src_stream = sys.stdin
@@ -39,7 +46,10 @@ def main() -> None:
         src_stream = open(args.filename, encoding='utf-8')
     with src_stream:
         src = src_stream.read()
-    print(interpret_expr(src))
+    if args.expression:
+        print(interpret_expr(src))
+    else:
+        interpret(src)
 
 if __name__ == '__main__':
     main()
