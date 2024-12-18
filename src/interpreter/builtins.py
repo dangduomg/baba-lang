850991160e3fb614a278f9db116f7fa52612691b
dangduomg <85876662+dangduomg@users.noreply.github.com>
2024-12-18 18:07:57 +0700

    refactor

--- src/interpreter/builtins.py
+++ src/interpreter/builtins.py
@@ -22,6 +22,7 @@ def print_(
     print(*(arg.to_string(meta).value for arg in args))
     return NULL
 
+
 def print_dump(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
@@ -32,6 +33,7 @@ def print_dump(
     print(*(arg.dump(meta) for arg in args))
     return NULL
 
+
 def input_(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
@@ -41,48 +43,56 @@ def input_(
     #pylint: disable=unused-argument
     return String(input(args[0].to_string(meta).value if args else ''))
 
+
 def int_(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
         /,
-        *args: Value
+        arg: Value,
+        *_: Value
 ) -> Int | BLError:
     #pylint: disable=unused-argument
-    match arg := args[0]:
+    match arg:
         case String(value=value) | Float(value=value):
             return Int(int(value))
         case Int():
             return arg
     return error_not_implemented.set_meta(meta)
 
+
 def float_(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
         /,
-        *args: Value
+        arg: Value,
+        *_: Value
 ) -> Float | BLError:
     #pylint: disable=unused-argument
-    match arg := args[0]:
+    match arg:
         case String(value=value) | Int(value=value):
             return Float(float(value))
         case Float():
             return arg
     return error_not_implemented.set_meta(meta)
 
+
 def bool_(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
         /,
-        *args: Value
+        arg: Value,
+        *_: Value
 ) -> Bool:
     #pylint: disable=unused-argument
-    return args[0].to_bool(meta)
+    return arg.to_bool(meta)
+
 
 def str_(
         meta: Optional[Meta],
         interpreter: 'ASTInterpreter',
         /,
-        *args: Value
+        arg: Value,
+        *_: Value
 ) -> String:
     #pylint: disable=unused-argument
-    return args[0].to_string(meta)
+    return arg.to_string(meta)
